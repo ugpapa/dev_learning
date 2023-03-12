7bbcb362f2b414eaf38b1845edba9efbafd0c499
Phuong Nguyen <pnha1303@gmail.com>
2023-03-12 08:46:06 +0700

    feat: clean up

--- pauline/09-March/Experience/Utils/Resources.js
+++ pauline/09-March/Experience/Utils/Resources.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { GLTFLoader } from 'https://unpkg.com/three@0.148.0/examples/jsm/loaders/GLTFLoader.js'
+import { GLTFLoader } from 'gltfLoader'
 import EventEmitter from './EventEmitter.js'
 
 export default class Resources extends EventEmitter
